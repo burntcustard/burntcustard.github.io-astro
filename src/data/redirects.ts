8bde0c49379a4e1646b3d54ac948f05de1d4b661
burntcustard <burntcustard@gmail.com>
2024-12-11 19:28:23 +0000

    Add a few redirects & a ERADMEtweak

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -1,10 +1,10 @@
-import { type AstroUserConfig } from "astro";
+import { type RedirectConfig } from "astro";
 
-const redirects: AstroUserConfig['redirects'] = {
-  'owlbear': {
-    status: 303,
-    destination: 'https://www.owlbear.rodeo/room/MTQZ-TR92MrX/TheNewGnome',
-  },
+const redirects: Record<string, RedirectConfig> = {
+  'bsky': 'https://bsky.app/profile/burnt.io',
+  'dod-doc': 'https://docs.google.com/document/d/1Tw8AcaAu7u-zTpa4sOMrGzMRBhxHCrHMVXr4oBsd_YM/edit',
+  'dod-invite': 'https://www.dndbeyond.com/campaigns/join/49020231970683115',
+  'owlbear': 'https://www.owlbear.rodeo/room/MTQZ-TR92MrX/TheNewGnome',
 }
 
 export default redirects;
