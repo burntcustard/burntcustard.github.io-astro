0dab2e36ef8ba71155db56d0c59ba3d719842739
burntcustard <burntcustard@gmail.com>
2024-12-12 00:46:29 +0000

    Try to configure site and base for previewing on GitHub pages before migrating to burnt.io v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import webmanifest from 'astro-webmanifest';
 import redirects from './src/data/redirects';
 
 export default defineConfig({
-  site: 'https://burnt.io/burcustard.github.io-astro',
+  site: 'https://burnt.io',
+  base: '/burcustard.github.io-astro',
   redirects,
   integrations: [
     mdx(),
