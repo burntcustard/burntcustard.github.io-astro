201cfe821da089784ee30203253d2d00069a2d35
burntcustard <burntcustard@gmail.com>
2025-01-22 07:49:12 +0000

    Pull h1s out of headers, tweak their styling, & minor exporty changes

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -1,10 +1,8 @@
 import { type RedirectConfig } from "astro";
 
-const redirects: Record<string, RedirectConfig> = {
+export const redirects: Record<string, RedirectConfig> = {
   'bsky': 'https://bsky.app/profile/burnt.io',
   'dod-doc': 'https://docs.google.com/document/d/1Tw8AcaAu7u-zTpa4sOMrGzMRBhxHCrHMVXr4oBsd_YM/edit',
   'dod-invite': 'https://www.dndbeyond.com/campaigns/join/49020231970683115',
   'owlbear': 'https://www.owlbear.rodeo/room/MTQZ-TR92MrX/TheNewGnome',
 }
-
-export default redirects;
